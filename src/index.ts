621f88df5290da0efe24bfe68d2927d57daeb3f9
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-03-15 21:01:20 -0300

    exercicio-de-fixacao-concluido

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
-import { accounts } from './database'
-import { ACCOUNT_TYPE, TAccount } from './types'
+import { accounts, cars, } from './database'
+import { ACCOUNT_TYPE, CAR_BRANDS, CAR_FUEL, CAR_TYPES, TAccount, TCars } from './types'
+
 
 const app = express()
 
@@ -20,7 +21,7 @@ app.get("/accounts", (req: Request, res: Response) => {
     res.send(accounts)
 })
 
-
+//Pratica Guiada
 app.get("/accounts/:id", (req: Request, res: Response) => {
     const id: string = req.params.id
 
@@ -71,9 +72,6 @@ app.delete("/accounts/:id", (req: Request, res: Response) => {
 
 })
 
-
-
-
 //Ordem dos Métodos
 //GET
 //POST
@@ -82,3 +80,71 @@ app.delete("/accounts/:id", (req: Request, res: Response) => {
 //Colocar os EndPoint com params sempre no final
 
 
+
+//Exercício de fixação feito na ordem certa
+
+//Endpoint que busca todos os cars
+app.get("/cars", (req: Request, res: Response)=>{
+    res.status(200).send(cars)
+})
+
+//Endpoint para cadastrar novo cars
+app.post("/cars", (req: Request, res: Response)=>{
+    const {id, brand, model, type, fuel} = req.body as TCars
+
+    const newCar = {
+        id,
+        brand,
+        model,
+        type,
+        fuel
+    }
+    cars.push(newCar)
+    res.status(201).send("Carro cadastrado com sucesso")
+})
+
+//Endpoint para editar dados do cars
+app.put("/cars/:id", (req: Request, res: Response)=>{
+    const id = req.params.id
+
+    const newId = req.body.id as string | undefined
+    const newBrand = req.body.brand as CAR_BRANDS | undefined
+    const newModel = req.body.model as string | undefined
+    const newType = req.body.type as CAR_TYPES |undefined
+    const newFuel = req.body.fuel as CAR_FUEL | undefined
+
+    const car = cars.find((car)=>{
+        return car.id === id
+    })
+
+    if(car){
+        car.id = newId || car.id
+        car.brand = newBrand || car.brand
+        car.model = newModel || car.model
+        car.type = newType || car.type
+        car.fuel = newFuel || car.fuel
+        res.status(200).send("Atualização realizada com sucesso")
+    }else{
+        res.status(404).send("Item não encontrado")
+    }
+})
+
+//Endpoint para deletar cars
+app.delete("/cars/:id", (req: Request, res: Response)=>{
+    const id = req.params.id
+    const carIndex = cars.findIndex((car) => {
+        return car.id === id
+    })
+    if (carIndex >= 0) {
+        cars.splice(carIndex, 1)
+        res.status(200).send("Item deletado com sucesso")
+    } else {
+        res.status(404).send("Item não encontrado")
+    }
+})
+
+//Ordem do CRUD
+// POST
+// GET
+// PUT
+// DELETE
